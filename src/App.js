5c1cf28fa9c905c120777446f5236417d644d53c
rockxxxxxx <78143717+rockxxxxxx@users.noreply.github.com>
2022-11-11 16:14:33 +0530

    added favicon

--- src/App.js
+++ src/App.js
@@ -12,6 +12,19 @@ import Expenses from "./component/expenses/Expenses";
 
 const SignUp = React.lazy(() => import("./component/signup/Signup"));
 
+var hours = 0.03; // to clear the localStorage after 1 hour
+// (if someone want to clear after 8hrs simply change hours=8)
+var now = new Date().getTime();
+var setupTime = localStorage.getItem("setupTime");
+if (setupTime == null) {
+  localStorage.setItem("setupTime", now);
+} else {
+  if (now - setupTime > hours * 60 * 60 * 1000) {
+    localStorage.clear();
+    localStorage.setItem("setupTime", now);
+  }
+}
+
 function App() {
   const { isLoggedIn } = useContext(LoginContext);
   return (
